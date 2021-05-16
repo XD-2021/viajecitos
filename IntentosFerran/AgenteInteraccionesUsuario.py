f996561849edce24433dfc652e97c3e153a4c456
Ferran Iglesias Barenys <74904071+Ferran00@users.noreply.github.com>
2021-05-16 16:39:16 +0200

    intent terrible de començar un agent ( AgenteInteraccionesUsuario.py)

---
+++ IntentosFerran/AgenteInteraccionesUsuario.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 27 15:58:13 2013
+
+Esqueleto de agente usando los servicios web de Flask
+
+/comm es la entrada para la recepcion de mensajes del agente
+/Stop es la entrada que para el agente
+
+Tiene una funcion AgentBehavior1 que se lanza como un thread concurrente
+
+Asume que el agente de registro esta en el puerto 9000
+
+@author: javier
+"""
+
+from multiprocessing import Process, Queue
+import socket
+
+from rdflib import Namespace, Graph
+from flask import Flask
+
+from AgentUtil.FlaskServer import shutdown_server
+from AgentUtil.Agent import Agent
+
+import requests
+from flask import Flask, request
+from requests import ConnectionError
+
+__author__ = 'Ferran'
+
+# Configuration stuff
+hostname = socket.gethostname()
+port = 9010
+
+agn = Namespace("http://www.agentes.org#")
+
+# Contador de mensajes
+mss_cnt = 0
+
+# Datos del Agente
+
+AgentePersonal = Agent('AgenteSimple',
+                       agn.AgenteSimple,
+                       'http://%s:%d/comm' % (hostname, port),
+                       'http://%s:%d/Stop' % (hostname, port))
+
+# Directory agent address
+DirectoryAgent = Agent('DirectoryAgent',
+                       agn.Directory,
+                       'http://%s:9000/Register' % hostname,
+                       'http://%s:9000/Stop' % hostname)
+
+# Global triplestore graph
+dsgraph = Graph()
+
+cola1 = Queue()
+
+# Flask stuff
+app = Flask(__name__)
+
+
+@app.route("/comm")
+def comunicacion():
+    """
+    Entrypoint de comunicacion
+    """
+    global dsgraph
+    global mss_cnt
+    pass
+
+    # copiat de arithmetic:
+    mess = request.args['message']
+
+    if '|' not in mess:
+        return 'ERROR: INVALID MESSAGE'
+    else:
+        # Sintaxis de los mensajes "TIPO|PARAMETROS"
+        messtype, messparam = mess.split('|')
+
+        if messtype not in ['PETICION']:    #peticion de planificacion de visita turistica
+            return 'ERROR: INVALID REQUEST'
+        else:
+            # parametros mensaje SOLVE = "SOLVERADDRESS,PROBID,PROB"
+            if messtype == 'PETICION':
+                #té les restricciones
+                restricciones = messparam.split(',')
+
+                #agafar de "restricciones" els 3 possibles elements: restricciones de actividades, de aloj y  de transporte. cadascuna de les 3 és un set de restriccions.
+
+                #p1 = Process(target=solver, args=(solveraddress, probid, prob))
+                #p1.start()
+                return 'OK'
+
+
+@app.route("/Stop")
+def stop():
+    """
+    Entrypoint que para el agente
+
+    :return:
+    """
+    tidyup()
+    shutdown_server()
+    return "Parando Servidor"
+
+
+def tidyup():
+    """
+    Acciones previas a parar el agente
+
+    """
+    pass
+
+
+def agentbehavior1(cola):
+    """
+    Un comportamiento del agente
+
+    :return:
+    """
+    pass
+
+
+if __name__ == '__main__':
+    # Ponemos en marcha los behaviors
+    ab1 = Process(target=agentbehavior1, args=(cola1,))
+    ab1.start()
+
+    # Ponemos en marcha el servidor
+    app.run(host=hostname, port=port)
+
+    # Esperamos a que acaben los behaviors
+    ab1.join()
+    print('The End')
